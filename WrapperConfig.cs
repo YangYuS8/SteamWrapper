a88252d46e8b79d0ad13f0a11c377c6412e5abe6
YangYuS8 <YangYuS8@163.com>
2026-07-09 17:31:53 +0800

    chore: remove legacy dotnet project artifacts

--- WrapperConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿public class WrapperConfig
-{
-    public string LauncherExe { get; set; } = "nine_kokoiro_chs.exe";
-    public string OriginalExe { get; set; } = "nine_kokoiro.exe";
-    public string Arguments { get; set; } = "";
-    public bool WaitForChildProcessTree { get; set; } = true;
-}
